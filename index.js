eaccc884535f92b35dc79027a83438f96da24bff
rebecca_huseyin <rebecca_huseyin@outlook.com>
2018-12-29 14:51:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidays () {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy ${holiday}, ${name}!`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday,name) {
